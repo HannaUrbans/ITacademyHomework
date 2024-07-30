7ccc22a6f599d375182a1cd5ff7d76ab0114804f
Hanna Urban <huan92@yandex.ru>
2024-07-30 16:30:15 +0300

    task 9: instanceOf modified into 3 final fields

--- src/home_work_3/calcs/additional/CalculatorWithCounterAutoChoiceAgregation.java
+++ src/home_work_3/calcs/additional/CalculatorWithCounterAutoChoiceAgregation.java
@@ -16,89 +16,98 @@ import home_work_3.calcs.simple.CalculatorWithOperator;
  * 	9.6 В классе должен быть метод long getCountOperation() который должен возвращать количество использований данного калькулятора. При вызове данного метода счётчик учёта не увеличивается.
  */
 public class CalculatorWithCounterAutoChoiceAgregation {
-    private final Object calculator;
+    // класс содержит отдельные поля для каждого типа калькулятора и проверяет, какой из них инициализирован
+    private final CalculatorWithOperator calcWithOperator;
+    private final CalculatorWithMathCopy calcWithMathCopy;
+    private final CalculatorWithMathExtends calcWithMathExtends;
     private long countOperation = 0;
 
     public CalculatorWithCounterAutoChoiceAgregation(CalculatorWithOperator calculator) {
-        this.calculator = calculator;
+        this.calcWithOperator = calculator;
+        this.calcWithMathCopy = null;
+        this.calcWithMathExtends = null;
     }
 
     public CalculatorWithCounterAutoChoiceAgregation(CalculatorWithMathCopy calculator) {
-        this.calculator = calculator;
+        this.calcWithOperator = null;
+        this.calcWithMathCopy = calculator;
+        this.calcWithMathExtends = null;
     }
 
     public CalculatorWithCounterAutoChoiceAgregation(CalculatorWithMathExtends calculator) {
-        this.calculator = calculator;
+        this.calcWithOperator = null;
+        this.calcWithMathCopy = null;
+        this.calcWithMathExtends = calculator;
     }
 
     public double divide(double a, double b) {
         countOperation++;
-        if (calculator instanceof CalculatorWithOperator) {
-            return ((CalculatorWithOperator) calculator).divide(a, b);
-        } else if (calculator instanceof CalculatorWithMathCopy) {
-            return ((CalculatorWithMathCopy) calculator).divide(a, b);
-        } else if (calculator instanceof CalculatorWithMathExtends) {
-            return ((CalculatorWithMathExtends) calculator).divide(a, b);
+        if (calcWithOperator != null) {
+            return calcWithOperator.divide(a, b);
+        } else if (calcWithMathCopy != null) {
+            return calcWithMathCopy.divide(a, b);
+        } else if (calcWithMathExtends != null) {
+            return calcWithMathExtends.divide(a, b);
         }
         return -1;
     }
 
     public double multiply(double a, double b) {
         countOperation++;
-        if (calculator instanceof CalculatorWithOperator) {
-            return ((CalculatorWithOperator) calculator).multiply(a, b);
-        } else if (calculator instanceof CalculatorWithMathCopy) {
-            return ((CalculatorWithMathCopy) calculator).multiply(a, b);
-        } else if (calculator instanceof CalculatorWithMathExtends) {
-            return ((CalculatorWithMathExtends) calculator).multiply(a, b);
+        if (calcWithOperator != null) {
+            return calcWithOperator.multiply(a, b);
+        } else if (calcWithMathCopy != null) {
+            return calcWithMathCopy.multiply(a, b);
+        } else if (calcWithMathExtends != null) {
+            return calcWithMathExtends.multiply(a, b);
         }
         return -1;
     }
 
     public double subtract(double a, double b) {
         countOperation++;
-        if (calculator instanceof CalculatorWithOperator) {
-            return ((CalculatorWithOperator) calculator).subtract(a, b);
-        } else if (calculator instanceof CalculatorWithMathCopy) {
-            return ((CalculatorWithMathCopy) calculator).subtract(a, b);
-        } else if (calculator instanceof CalculatorWithMathExtends) {
-            return ((CalculatorWithMathExtends) calculator).subtract(a, b);
+        if (calcWithOperator != null) {
+            return calcWithOperator.subtract(a, b);
+        } else if (calcWithMathCopy != null) {
+            return calcWithMathCopy.subtract(a, b);
+        } else if (calcWithMathExtends != null) {
+            return calcWithMathExtends.subtract(a, b);
         }
         return -1;
     }
 
     public double add(double a, double b) {
         countOperation++;
-        if (calculator instanceof CalculatorWithOperator) {
-            return ((CalculatorWithOperator) calculator).add(a, b);
-        } else if (calculator instanceof CalculatorWithMathCopy) {
-            return ((CalculatorWithMathCopy) calculator).add(a, b);
-        } else if (calculator instanceof CalculatorWithMathExtends) {
-            return ((CalculatorWithMathExtends) calculator).add(a, b);
+        if (calcWithOperator != null) {
+            return calcWithOperator.add(a, b);
+        } else if (calcWithMathCopy != null) {
+            return calcWithMathCopy.add(a, b);
+        } else if (calcWithMathExtends != null) {
+            return calcWithMathExtends.add(a, b);
         }
         return -1;
     }
 
     public double raiseToPower(double a, double b) {
         countOperation++;
-        if (calculator instanceof CalculatorWithOperator) {
-            return ((CalculatorWithOperator) calculator).raiseToPower(a, b);
-        } else if (calculator instanceof CalculatorWithMathCopy) {
-            return ((CalculatorWithMathCopy) calculator).raiseToPower(a, b);
-        } else if (calculator instanceof CalculatorWithMathExtends) {
-            return ((CalculatorWithMathExtends) calculator).raiseToPower(a, b);
+        if (calcWithOperator != null) {
+            return calcWithOperator.raiseToPower(a, b);
+        } else if (calcWithMathCopy != null) {
+            return calcWithMathCopy.raiseToPower(a, b);
+        } else if (calcWithMathExtends != null) {
+            return calcWithMathExtends.raiseToPower(a, b);
         }
         return -1;
     }
 
     public double absoluteValue(double a) {
         countOperation++;
-        if (calculator instanceof CalculatorWithOperator) {
-            return ((CalculatorWithOperator) calculator).absoluteValue(a);
-        } else if (calculator instanceof CalculatorWithMathCopy) {
-            return ((CalculatorWithMathCopy) calculator).absoluteValue(a);
-        } else if (calculator instanceof CalculatorWithMathExtends) {
-            return ((CalculatorWithMathExtends) calculator).absoluteValue(a);
+        if (calcWithOperator != null) {
+            return calcWithOperator.absoluteValue(a);
+        } else if (calcWithMathCopy != null) {
+            return calcWithMathCopy.absoluteValue(a);
+        } else if (calcWithMathExtends != null) {
+            return calcWithMathExtends.absoluteValue(a);
         }
         return -1;
     }
@@ -106,12 +115,12 @@ public class CalculatorWithCounterAutoChoiceAgregation {
 
     public double squareRoot(double a) {
         countOperation++;
-        if (calculator instanceof CalculatorWithOperator) {
-            return ((CalculatorWithOperator) calculator).squareRoot(a);
-        } else if (calculator instanceof CalculatorWithMathCopy) {
-            return ((CalculatorWithMathCopy) calculator).squareRoot(a);
-        } else if (calculator instanceof CalculatorWithMathExtends) {
-            return ((CalculatorWithMathExtends) calculator).squareRoot(a);
+        if (calcWithOperator != null) {
+            return calcWithOperator.squareRoot(a);
+        } else if (calcWithMathCopy != null) {
+            return calcWithMathCopy.squareRoot(a);
+        } else if (calcWithMathExtends != null) {
+            return calcWithMathExtends.squareRoot(a);
         }
         return -1;
     }
