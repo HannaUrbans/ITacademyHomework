a85ed133c50fba140963f22e9b8b4a6aef046d8e
Hanna Urban <huan92@yandex.ru>
2024-08-06 00:46:29 +0300

    OddOrEven corrected

--- src/home_work_1/OddOrEven.java
+++ src/home_work_1/OddOrEven.java
@@ -4,26 +4,37 @@ import java.util.Scanner;
 
 public class OddOrEven {
     public static void main(String[] args) {
+        OddOrEven printer = new OddOrEven();
+
         Scanner in = new Scanner(System.in);
 
         System.out.println("Введите целое число в диапазоне от 0 до 2 147 483 647.");
         int inputNumber = in.nextInt();
+        in.close();
 
+        System.out.print("Введённое число " + inputNumber);
+        String result = findOddOrEvenNumber(inputNumber);
+
+        System.out.println(result);
+    }
+
+    public static String findOddOrEvenNumber(int inputNumber) {
+        String answer;
         if (isInRange(inputNumber)) {
-            if (inputNumber % 2 == 0) {
-                System.out.println("Число " + inputNumber + " чётное.");
+            if (inputNumber % 2 != 0) {
+                answer = " нечётное";
             } else {
-                System.out.println("Число " + inputNumber + " НЕчётное.");
+                answer = " чётное";
             }
         } else {
-            System.out.println("Введённое число не входит в требуемый диапазон.");
+            answer = " не входит в требуемый диапазон.";
         }
-        in.close();
+        return answer;
     }
 
     public static boolean isInRange(int number) {
         return number >= 0;
-        }
     }
+}
 
 
