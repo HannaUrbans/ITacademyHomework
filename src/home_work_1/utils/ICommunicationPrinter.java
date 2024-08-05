fb351940759f4e2019b889b4c79dc9b7561f6f82
Hanna Urban <huan92@yandex.ru>
2024-08-06 00:48:09 +0300

    homework1 interface added

---
+++ src/home_work_1/utils/ICommunicationPrinter.java
@@ -0,0 +1,11 @@
+package home_work_1.utils;
+
+public interface ICommunicationPrinter {
+    /**
+     * Возвращает приветственное сообщение для указанного имени.
+     *
+     * @param name Имя пользователя.
+     * @return Приветственное сообщение.
+     */
+   String welcom(String name);
+}
