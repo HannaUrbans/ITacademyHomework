d9fb2764a22e8ab640076ccef25df11e22e152c1
Hanna Urban <huan92@yandex.ru>
2024-08-05 15:41:06 +0300

    GreetingIfElse corrected

--- src/home_work_1/GreetingIfElse.java
+++ src/home_work_1/GreetingIfElse.java
@@ -1,9 +1,13 @@
 package home_work_1;
 
+import home_work_1.utils.ICommunicationPrinter;
+
+import java.util.Objects;
 import java.util.Scanner;
 
-public class GreetingIfElse {
+public class GreetingIfElse implements ICommunicationPrinter {
     public static void main(String[] args) {
+        GreetingIfElse printer = new GreetingIfElse();
         Scanner in = new Scanner(System.in);
         System.out.println("Введите Ваше имя");
         String inputName = in.nextLine().trim(); //!вводим текст - убери пробелы
@@ -12,20 +16,24 @@ public class GreetingIfElse {
         if (inputName.isEmpty()) {
             System.out.println("Вы ничего не ввели.");
             return;
+        } else{
+            System.out.println(printer.welcom(inputName));
         }
+    }
 
-        String userVasya = "Вася";
-        String userAnastasiya = "Анастасия";
-
-        if (inputName.equals(userVasya)) {
-            System.out.println("Привет!\nЯ тебя так долго ждал");
-        } else {
-            if (inputName.equals(userAnastasiya)) {
-                System.out.println("Я тебя так долго ждал");
-            } else {
-                System.out.println("Добрый день, а вы кто?");
+    @Override
+    public String welcom(String name) {
+        String message = "";
+        if (Objects.equals(name, "Вася")) {
+            message = "Привет!\nЯ тебя так долго ждал";
+        } else{
+            if (Objects.equals(name, "Анастасия")) {
+                message = "Я тебя так долго ждал";
+            } else{
+                message = "Добрый день, а вы кто?";
             }
         }
+        return message;
     }
 }
 
