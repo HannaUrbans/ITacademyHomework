d9afb5a3175e827054b259506f6f3e8ba88fb335
Hanna Urban <huan92@yandex.ru>
2024-08-05 16:13:39 +0300

    GreetingSwitch corrected

--- src/home_work_1/GreetingSwitch.java
+++ src/home_work_1/GreetingSwitch.java
@@ -1,9 +1,10 @@
 package home_work_1;
 
-import java.util.Scanner;
+import home_work_1.utils.ICommunicationPrinter;
 
-public class GreetingSwitch {
-    public static void main(String[] args) {
+public class GreetingSwitch implements ICommunicationPrinter {
+    /*public static void main(String[] args) {
+        GreetingSwitch printer = new GreetingSwitch();
         Scanner in = new Scanner(System.in);
         System.out.println("Введите Ваше имя");
         String inputName = in.nextLine().trim();
@@ -12,17 +13,26 @@ public class GreetingSwitch {
         if (inputName.isEmpty()) {
             System.out.println("Вы ничего не ввели.");
             return;
+        } else {
+            System.out.println(printer.welcom(inputName));
         }
-        switch (inputName) {
-            case "Вася":
-                System.out.println("Привет!\nЯ тебя так долго ждал");
-                break;
-            case "Анастасия":
-                System.out.println("Я тебя так долго ждал");
-                break;
-            default:
-                System.out.println("Добрый день, а вы кто?");
-                break;
-        }
+    }*/
+
+    @Override
+     public String welcom (String name){
+        String message = "";
+
+        switch(name){
+        case "Вася":
+            message = "Привет!\nЯ тебя так долго ждал";
+            break;
+        case "Анастасия":
+            message = "Я тебя так долго ждал";
+            break;
+        default:
+            message = "Добрый день, а вы кто?";
+            break;
+    }
+    return message;
     }
 }
