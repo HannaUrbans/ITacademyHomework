4f1bf40510ea24d8626b95ddb1b3f612ab2435db
Hanna Urban <huan92@yandex.ru>
2024-08-05 14:49:24 +0300

    test folder added

---
+++ src/test/CalculatorWithOperatorTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import home_work_3.calcs.simple.CalculatorWithOperator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorWithOperatorTest {
+    private final CalculatorWithOperator calculator = new CalculatorWithOperator();
+
+    @Test
+    public void test1() {
+        Assertions.assertEquals(2, calculator.add(1, 1));
+    }
+
+    @Test
+    public void test2() {
+        Assertions.assertEquals(0, calculator.subtract(1, 1));
+    }
+}
