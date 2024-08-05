6518fa1657670fb9ffc867bfffb68ac4e4b8b535
Hanna Urban <huan92@yandex.ru>
2024-08-06 01:35:31 +0300

    IsDivided altered

--- src/home_work_1/IsDivided.java
+++ src/home_work_1/IsDivided.java
@@ -6,7 +6,7 @@ public class IsDivided {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Введите первое целое число в диапазоне от 1 до 2 147 483 647."); //0 не входит в диапазон
+        System.out.println("Введите целое число (делитель) в диапазоне от 1 до 2 147 483 647."); //0 не входит в диапазон
         int inputNumber1 = in.nextInt();
 
         if (!isInRange(inputNumber1)) {
@@ -14,23 +14,26 @@ public class IsDivided {
             return;
         }
 
-        System.out.println("Введите второе целое число в диапазоне от 1 до 2 147 483 647.");
+        System.out.println("Введите целое число (делимое) в диапазоне от 1 до 2 147 483 647.");
         int inputNumber2 = in.nextInt();
         if (!isInRange(inputNumber2)) {
             System.out.println("Введённое число не входит в требуемый диапазон.");
             return;
         }
-
-        if ((inputNumber2 > inputNumber1) && (inputNumber2 % inputNumber1 == 0)) {
-            System.out.println("Число " + inputNumber2 + " делится на число " + inputNumber1 + " без остатка.");
-        } else if ((inputNumber2 < inputNumber1) && (inputNumber1 % inputNumber2 == 0)) {
-            System.out.println("Число " + inputNumber1 + " делится на число " + inputNumber2 + " без остатка.");
-        } else if (inputNumber2 == inputNumber1) {
-            System.out.println("Число " + inputNumber1 + " делится на число " + inputNumber2 + " без остатка.");
-        } else {
-            System.out.println("Числа не делятся без остатка.");
-        }
         in.close();
+
+        System.out.print("Число " + inputNumber2 + " делится на число " + inputNumber1);
+        System.out.println(findIfDivided(inputNumber1, inputNumber2));
+    }
+
+    public static String findIfDivided (int a, int b){
+        String answer;
+        if ((b > a) && (b % a == 0) || b == a) {
+            answer = " без остатка.";
+        } else {
+            answer = " с остатком.";
+        }
+        return answer;
     }
 
     public static boolean isInRange(int number) {
