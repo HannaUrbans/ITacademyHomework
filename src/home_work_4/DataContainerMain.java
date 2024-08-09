e40855e43041aeb3e723461341e647d4b3c02ede
Hanna Urban <huan92@yandex.ru>
2024-08-09 16:40:37 +0300

    main checked toString

--- src/home_work_4/DataContainerMain.java
+++ src/home_work_4/DataContainerMain.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class DataContainerMain {
     public static void main(String[] args) {
         int arrayLength = 10;
-       //DataContainer<String> container = new DataContainer<>(String.class, new String[arrayLength]);
-       DataContainer<String> container = new DataContainer<>(String.class);
+        //DataContainer<String> container = new DataContainer<>(String.class, new String[arrayLength]);
+        DataContainer<String> container = new DataContainer<>(String.class);
 
         int index1 = container.add("Привет");
         int index2 = container.add("Как дела");
@@ -15,11 +15,11 @@ public class DataContainerMain {
 
         String text1 = container.get(index1);
         String text2 = container.get(index2);
-       String text3 = container.get(index3);
+        String text3 = container.get(index3);
         String text4 = container.get(index4);
         System.out.println(text1); //Привет
         System.out.println(text2); //Как дела
-       System.out.println(text3); //Работаю
+        System.out.println(text3); //Работаю
         System.out.println(text4); //Давай потом
         System.out.println(container.delete(text1));
         System.out.println(container.delete("Как дела"));
@@ -27,19 +27,20 @@ public class DataContainerMain {
         System.out.println(Arrays.toString(container.getItems())); // ["Работаю", "Как дела", "Давай потом"]
 
         // Создание контейнера с начальными значениями
-        Integer[] initialArray = {1, 2, 3, 777};
+        Integer[] initialArray = {1, 2, null, 3, 777, null};
         //Integer[] initialArray = {};
         DataContainer<Integer> container2 = new DataContainer<>(Integer.class, initialArray);
 
         // Получение значений
         // Integer может иметь null внутри
-       Integer num1 = container2.get(0);
+        Integer num1 = container2.get(0);
         Integer num2 = container2.get(1);
         Integer num3 = container2.get(2);
         Integer num4 = container2.get(3);
-       boolean deletePrint = container2.delete((Integer)777);
+        //boolean deletePrint = container2.delete((Integer)777);
         System.out.println(Arrays.toString(container2.getItems()));
-        System.out.println(deletePrint);
+        // System.out.println(deletePrint);
+        System.out.println(container2.toString());
         //System.out.println(Arrays.toString(container2.getItems()));
 
         // DataContainer<Integer> container2 = new DataContainer<>(Integer.class, new Integer[arrayLength]);
